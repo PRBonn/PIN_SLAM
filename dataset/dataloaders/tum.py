[MINOR] correct rgb-to-intensity formula, fix tum dataloader

dataset/dataloaders/tum.py
@@ -169,7 +169,7 @@ class TUMDataset:
         frame_data = {"points": points_xyzrgb}
 
         if self.load_img:
-            rgb_image = np.array(rgb_image)
+            rgb_image = np.array(self.rgb_frames[idx])
             rgb_image_dict = {"cam": rgb_image}
             frame_data["img"] = rgb_image_dict
 

utils/tools.py
@@ -396,7 +396,7 @@ def write_to_json(filename: Path, content: dict):
 
 
 def color_to_intensity(colors: torch.tensor):
-    intensity = 0.144 * colors[:, 0] + 0.299 * colors[:, 1] + 0.587 * colors[:, 2]
+    intensity = 0.299 * colors[:, 0] + 0.587 * colors[:, 1] + 0.114 * colors[:, 2] # thanks @zhSlamer (issue #46)
     return intensity.unsqueeze(1)
 
 